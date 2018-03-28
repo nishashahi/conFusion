f558fc32c0b56e4f4aff6533427cfb59cf580d91
nishashahi <nisha111shahi@gmail.com>
2018-03-28 20:45:43 +0545

    Angular Component part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule, MdButtonModule, MdSelectModule } from '@angular/material'; 
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MdButtonModule,
+    MdSelectModule,  
+    MaterialModule, 
+    FlexLayoutModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
